4b5c443c8b9f21e20e45c991a16823ad918eee94
Ritual Yang <995022373@qq.com>
2019-11-01 20:55:47 +0800

    使用druid数据库连接池

--- springboot-parent/springboot-mybatis/mybatis-xml/src/main/java/com/ritualyang/mapper/UserMapper.java
+++ springboot-parent/springboot-mybatis/mybatis-xml/src/main/java/com/ritualyang/mapper/UserMapper.java
@@ -2,12 +2,15 @@ package com.ritualyang.mapper;
 
 import com.ritualyang.model.User;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.common.Mapper;
 
 /**
  * @author RitualYang
  * @create 2019-11-01 17:08
  */
+@Transactional
 public interface UserMapper {
 
     User selectOneUser(String username);
