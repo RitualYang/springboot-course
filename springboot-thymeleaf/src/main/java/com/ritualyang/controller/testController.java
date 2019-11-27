d742b52fa945aec1da5b7f41e475445a704153ba
RitualYang <995022373@qq.com>
2019-11-27 23:03:35 +0800

    thymeleaf简单整合

---
+++ springboot-thymeleaf/src/main/java/com/ritualyang/controller/testController.java
@@ -0,0 +1,22 @@
+package com.ritualyang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author RitualYang
+ * @create 2019-11-27 22:51
+ */
+@Controller
+public class testController {
+
+    @RequestMapping(value = "show", method = RequestMethod.GET)
+    public String show(Model model){
+        model.addAttribute("uid","123456");
+        model.addAttribute("name","RitualYang");
+        return "show";
+    }
+
+}
---
+++ springboot-thymeleaf/src/main/java/com/ritualyang/thymeleafApplication.java
@@ -0,0 +1,15 @@
+package com.ritualyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RitualYang
+ * @create 2019-11-27 22:50
+ */
+@SpringBootApplication
+public class thymeleafApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(thymeleafApplication.class,args);
+    }
+}
