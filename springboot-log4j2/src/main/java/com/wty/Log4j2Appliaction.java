2b5f36101deda60f8f5a125efd424803a1742c93
RitualYang <995022373@qq.com>
2019-12-07 23:48:38 +0800

    整合log4j2

---
+++ springboot-log4j2/src/main/java/com/wty/Log4j2Appliaction.java
@@ -0,0 +1,15 @@
+package com.wty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RitualYang
+ * @create 2019-12-07 21:54
+ */
+@SpringBootApplication
+public class Log4j2Appliaction {
+    public static void main(String[] args) {
+        SpringApplication.run(Log4j2Appliaction.class,args);
+    }
+}
