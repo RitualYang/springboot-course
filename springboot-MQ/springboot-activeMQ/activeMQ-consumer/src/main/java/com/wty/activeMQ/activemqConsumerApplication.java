db2deca909bc4f97b21537e0397cf754b785a33b
RitualYang <995022373@qq.com>
2019-12-10 19:44:56 +0800

    整合消息队列activeMq

---
+++ springboot-MQ/springboot-activeMQ/activeMQ-consumer/src/main/java/com/wty/activeMQ/activemqConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.wty.activeMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class activemqConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(activemqConsumerApplication.class,args);
+    }
+}
---
+++ springboot-MQ/springboot-activeMQ/activeMQ-provider/src/main/java/com/wty/activeMQ/activemqProducerApplication.java
@@ -0,0 +1,11 @@
+package com.wty.activeMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class activemqProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(activemqProducerApplication.class,args);
+    }
+}
