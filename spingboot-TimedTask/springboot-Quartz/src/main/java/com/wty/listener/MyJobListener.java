e85de542ca79ecbfa85edcab93db3095a91f1b3d
RitualYang <995022373@qq.com>
2019-12-18 14:29:06 +0800

    监听与监听测试类添加

---
+++ spingboot-TimedTask/springboot-Quartz/src/main/java/com/wty/listener/MyJobListener.java
@@ -0,0 +1,34 @@
+package com.wty.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.JobListener;
+
+@Slf4j
+public class MyJobListener implements JobListener {
+    @Override
+    public String getName() {
+        String name = getClass().getSimpleName();
+        log.info("获取监听器名称：" + name);
+        return name;
+    }
+
+    @Override
+    public void jobToBeExecuted(JobExecutionContext context) {
+        String jobName = context.getJobDetail().getKey().getName();
+        log.info(jobName + "——————任务即将被执行");
+    }
+
+    @Override
+    public void jobExecutionVetoed(JobExecutionContext context) {
+        String jobName = context.getJobDetail().getKey().getName();
+        log.info(jobName + "——————任务被否决");
+    }
+
+    @Override
+    public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
+        String jobName = context.getJobDetail().getKey().getName();
+        log.info(jobName + "——————任务执行完毕");
+    }
+}
---
+++ spingboot-TimedTask/springboot-Quartz/src/test/java/com/wty/test/ListenerTest.java
@@ -0,0 +1,78 @@
+package com.wty.test;
+
+
+import com.wty.job.QuartzJob;
+import com.wty.listener.MyJobListener;
+import com.wty.listener.MyTriggerListener;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.quartz.impl.matchers.EverythingMatcher;
+import org.quartz.impl.matchers.KeyMatcher;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.EventListener;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ListenerTest {
+
+    @Test
+    public void testJobListener() throws SchedulerException {
+
+        JobDetail quartzJob = JobBuilder.newJob(QuartzJob.class)
+                .withIdentity("quartzJob")
+                .storeDurably()
+                .build();
+        SimpleTrigger quartzTrigger = TriggerBuilder.newTrigger()
+                .withIdentity("quartzTrigger")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(2)
+                        .repeatForever()).build();
+
+        SchedulerFactory factory = new StdSchedulerFactory();
+
+        Scheduler scheduler = factory.getScheduler();
+        //绑定关系：可以1：N
+        scheduler.scheduleJob(quartzJob,quartzTrigger);
+
+        //创建并注册一个全局的Job Listener
+        scheduler.getListenerManager().addJobListener(new MyJobListener(), EverythingMatcher.allJobs());
+
+
+        //启动
+        scheduler.start();
+    }
+
+    @Test
+    public void testTriggerListener() throws SchedulerException {
+
+        JobDetail quartzJob = JobBuilder.newJob(QuartzJob.class)
+                .withIdentity("quartzJob")
+                .storeDurably()
+                .build();
+        SimpleTrigger quartzTrigger = TriggerBuilder.newTrigger()
+                .withIdentity("quartzTrigger")
+                .startNow()
+                .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                        .withIntervalInSeconds(2)
+                        .repeatForever()).build();
+
+        SchedulerFactory factory = new StdSchedulerFactory();
+
+        Scheduler scheduler = factory.getScheduler();
+        //绑定关系：可以1：N
+        scheduler.scheduleJob(quartzJob,quartzTrigger);
+
+        //创建并注册一个全局的Trigger Listener
+        scheduler.getListenerManager().addTriggerListener(new MyTriggerListener(), EverythingMatcher.allTriggers());
+
+        //启动
+        scheduler.start();
+    }
+
+
+}
