9c7c245d37434d8bb351da0fb01a524d4c3b861e
RitualYang <995022373@qq.com>
2019-12-24 14:35:26 +0800

    整合elasticsearch

---
+++ springboot-elasticsearch-7.2.0/src/main/java/com/wty/Elasticsearch7Application.java
@@ -0,0 +1,15 @@
+package com.wty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author RitualYang
+ * @create 2019-12-24 12:52
+ */
+@SpringBootApplication
+public class Elasticsearch7Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Elasticsearch7Application.class,args);
+    }
+}
