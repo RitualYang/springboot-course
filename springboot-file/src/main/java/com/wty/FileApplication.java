3e4dd3de31589c660770662638697eb239d2c7ca
RitualYang <995022373@qq.com>
2020-08-26 17:35:16 +0800

    springboot整合扩展

---
+++ springboot-file/src/main/java/com/wty/FileApplication.java
@@ -0,0 +1,17 @@
+package com.wty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author WTY
+ * @Date 2020/8/26 17:16
+ */
+@SpringBootApplication
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class,args);
+    }
+}
---
+++ springboot-file/src/main/java/com/wty/controller/FileController.java
@@ -0,0 +1,16 @@
+package com.wty.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @author WTY
+ * @Date 2020/8/26 17:17
+ */
+@RestController
+@RequestMapping("/file")
+public class FileController {
+
+}
