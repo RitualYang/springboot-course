a637e89d8fdc76e75d7a52f4faed61a36bd84dff
RitualYang <995022373@qq.com>
2020-08-26 22:02:48 +0800

    actuator监视整合

---
+++ springboot-actuator/src/main/java/com/wty/ActuatorApplication.java
@@ -0,0 +1,17 @@
+package com.wty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author WTY
+ * @Date 2020/8/26 21:07
+ */
+@SpringBootApplication
+public class ActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class,args);
+    }
+}
