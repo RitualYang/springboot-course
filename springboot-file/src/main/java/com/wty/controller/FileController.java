09e88e2da315a8b8f1dbee6c17ebac5c129f24dd
RitualYang <995022373@qq.com>
2020-08-27 09:57:26 +0800

    文件上传简单整合

--- springboot-file/src/main/java/com/wty/controller/FileController.java
+++ springboot-file/src/main/java/com/wty/controller/FileController.java
@@ -1,7 +1,8 @@
 package com.wty.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * TODO
@@ -9,8 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @author WTY
  * @Date 2020/8/26 17:17
  */
+@Slf4j
 @RestController
 @RequestMapping("/file")
 public class FileController {
+
+    @PostMapping("/upload")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+        String fileName = file.getOriginalFilename();
+        log.info("上传文件信息:{}",fileName);
+        return "上传成功";
+    }
+    @PostMapping("/uploads")
+    public String fileUpload(@RequestParam("files")MultipartFile[] files){
+        if (files == null || files.length == 0){
+            return "上传失败,请重新选择文件";
+        }
+        // 多文件上传
+        for (MultipartFile file : files){
+            String originalFilename = file.getOriginalFilename();
+            log.info("文件名:{}",originalFilename);
+        }
+        return "上传成功";
+    }
 
 }
